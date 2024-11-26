158d97b370419afca44cc9744ec3269a440f52e6
Sebastian Jensen <apps@tsjdev-apps.de>
2024-11-26 12:00:30 +0100

    Create Statics.cs

---
+++ src/RealtimeDataOpenAI/Utils/Statics.cs
@@ -0,0 +1,40 @@
+﻿namespace RealtimeDataOpenAI.Utils;
+
+internal static class Statics
+{
+    /// <summary>
+    ///     The key for Azure OpenAI.
+    /// </summary>
+    public const string AzureOpenAIKey
+        = "Azure OpenAI";
+
+    /// <summary>
+    ///     The key for OpenAI.
+    /// </summary>
+    public const string OpenAIKey
+        = "OpenAI";
+
+    /// <summary>
+    ///     The key for GPT-4o.
+    /// </summary>
+    public const string GPT4oKey
+        = "gpt-4o";
+
+    /// <summary>
+    ///     The key for GPT-4o.
+    /// </summary>
+    public const string GPT4oMiniKey
+        = "gpt-4o-mini";
+
+    /// <summary>
+    ///     The key for GPT-4-Turbo.
+    /// </summary>
+    public const string GPT4TurboKey
+        = "gpt-4-turbo";
+
+    /// <summary>
+    ///     The key for GPT-4.
+    /// </summary>
+    public const string GPT4Key
+        = "gpt-4";
+}
